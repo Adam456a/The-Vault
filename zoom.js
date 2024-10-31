10d42eb4d40685d4accf2989745c3adf3e980af1
AdamNB <104266798+Adam456a@users.noreply.github.com>
2024-10-31 15:42:04 +0000

    more n64 roms

--- zoom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const img = document.querySelector('.zoomable');
-    
-    img.addEventListener('click', function() {
-        img.classList.toggle('zoomed');
-    });
-});
